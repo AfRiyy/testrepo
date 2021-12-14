58ed0e42893a11cda76d9a57dc2be476ce83c7c4
AfRiyy <afriyy@gmail.com>
2021-12-14 20:40:36 +0100

    Pets fillable

--- api/draft/shelter/app/Models/Pets.php
+++ api/draft/shelter/app/Models/Pets.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Pets extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name', 'species', 'age', 'gender', 'adopted', 'picturePath'
+    ];
 }
