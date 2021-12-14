5275de8b59f27d9059a8cb5ccd6857aaaee272dc
AfRiyy <afriyy@gmail.com>
2021-12-14 20:29:33 +0100

    Api /pets útvonal

--- api/draft/shelter/routes/api.php
+++ api/draft/shelter/routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Models\Pets;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+Route::get('/pets',function(){
+    return Pets::all();
 });
