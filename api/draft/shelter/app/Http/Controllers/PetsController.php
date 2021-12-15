38d64cbf19b4a91251c10a1552100dea6242c2c3
AfRiyy <afriyy@gmail.com>
2021-12-15 14:53:07 +0100

    PetsController létrehozva

---
+++ api/draft/shelter/app/Http/Controllers/PetsController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PetsController extends Controller
+{
+    function index(){
+        
+    }
+    function create(){
+
+    }
+    function edit($Pet){
+
+    }
+}
+
