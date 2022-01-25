d6ffaf20df7771c0ae307ae98283edda9ff004b8
AfRiyy <madaraszdavidd@gmail.com>
2022-01-25 20:20:17 +0100

    Pet resources létrehozva

---
+++ api/shelter/app/Http/Resources/Pet.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class Pet extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return[
+            "id"=>$this->id,
+            "name"=>$this->name,
+            "species_id"->$this->species_id,
+            "breeds_id"->$this->breeds_id,
+            "age"->$this->age,
+            "gender"->$this->gender,
+            "adopted"->$this->adopted,
+            "locations_id"->$this->locations_id,
+            "picturePath"->$this->picturePath,
+            "neutered"->$this->neutered,
+            "created_at"->$this->created_at->format("m/d/Y"),
+            "updated_at"->$this->updated_at->format("m/d/Y")
+        ];
+    }
+}
