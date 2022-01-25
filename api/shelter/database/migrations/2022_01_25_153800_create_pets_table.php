f4bd6bcd2b6ee8bfa40a1fd54216734e6206b002
AfRiyy <madaraszdavidd@gmail.com>
2022-01-25 19:53:18 +0100

    Migrációs fájlok

--- api/shelter/database/migrations/2022_01_25_153800_create_pets_table.php
+++ api/shelter/database/migrations/2022_01_25_153800_create_pets_table.php
@@ -14,13 +14,20 @@ class CreatePetsTable extends Migration
     public function up()
     {
         Schema::create('pets', function (Blueprint $table) {
+            $table->engine = "InnoDb";
+            $table->charset = "utf8mb4";
+            $table->collation = "utf8mb4_hungarian_ci";
             $table->id();
             $table->string('name');
-            $table->string('species');
+            $table->foreignId('species_id');
+            $table->foreignId('breeds_id');
             $table->integer('age');
-            $table->enum('gender', array('hím', 'nőstény'));
+            $table->boolean('gender');
             $table->boolean('adopted');
+            $table->foreignId('locations_id');
             $table->string('picturePath');
+            $table->Date('entryDate');
+            $table->boolean('neutered');
             $table->timestamps();
         });
     }
