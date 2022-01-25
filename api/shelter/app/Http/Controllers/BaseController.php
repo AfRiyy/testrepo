ff579286d5507c95b44056d3f638351489d85ec1
AfRiyy <madaraszdavidd@gmail.com>
2022-01-25 19:51:11 +0100

    BaseController sendError metódus

--- api/shelter/app/Http/Controllers/BaseController.php
+++ api/shelter/app/Http/Controllers/BaseController.php
@@ -16,4 +16,18 @@ class BaseController extends Controller
         ];
         return response()->json( $response, 200 );
     }
+    public function sendError( $error, $errorMessages = [], $code = 404 ) {
+
+        $response = [
+
+            "success" => false,
+            "message" => $error,
+        ];
+
+        if( !empty( $errorMessages )) {
+
+            $response[ "data" ] = $errorMessages;
+        }
+        return response()->json( $response, $code );
+    }
 }
