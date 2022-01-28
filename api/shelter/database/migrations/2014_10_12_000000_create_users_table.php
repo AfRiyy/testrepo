c611d777f9ceb8582fb133462e0d585aec4cd4c5
AfRiyy <madaraszdavidd@gmail.com>
2022-01-28 17:32:34 +0100

    user table módosítva

--- api/shelter/database/migrations/2014_10_12_000000_create_users_table.php
+++ api/shelter/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,14 +14,18 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
+            $table->engine = "InnoDb";
+            $table->charset = "utf8mb4";
+            $table->collation = "utf8mb4_hungarian_ci";
             $table->id();
-            $table->string('username')->unique();
-            $table->string('fullName')->nullable();
-            $table->date('birth_date')->nullable();
+            $table->string('username', 10)->unique();
+            $table->string('full_name',80)->nullable();
+            $table->date('birth');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password', 16);
             $table->string('phone')->nullable();
+            $table->boolean('admin')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
