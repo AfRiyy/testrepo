510161e874bc26db09c7281408c14c77ed8d2483
AfRiyy <madaraszdavidd@gmail.com>
2022-01-28 13:41:14 +0100

    PetsController módosítva

--- api/shelter/app/Http/Controllers/PetsController.php
+++ api/shelter/app/Http/Controllers/PetsController.php
@@ -5,34 +5,47 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Controllers\BaseController as BaseController;
 use App\Models\Pets;
+use App\Models\Breed;
+use App\Models\Specie;
 use Validator;
 use App\Http\Resources\Pet as PetResources;
 use Illuminate\Support\Facades\DB;
+use Storage;
+use Illuminate\Support\Facades\Input;
 class PetsController extends BaseController
 {
     public function index(){
-        $pets = Pets::all();
-        return $this->belongsTo(Pets::class);
-        // return $this->sendResponse(PetResources::collection($pets),"Pets fetched");
+        $pets = DB::table('pets')
+            ->join('breeds','pets.breeds_id','=','breeds.id')
+            ->join('species','breeds.species_id','=','species.id')
+            ->select('pets.id','name','bname','sname','age','gender','adopted','shelters_id','picture_path','neutered','created_at','updated_at')
+            ->get();
+        return $this->sendResponse($pets, "Pets fetched");
     }
 
     public function create(Request $request){
         request()->validate([
             'name'=>'required',
-            'species'=>'required',
+            'breeds_id'=>'required',
             'age'=>'required',
             'gender'=>'required',
             'adopted'=>'required',
-            'picturePath'=>'required',
+            'picture_path'=>'required',
+            'neutered'=>'required',
         ]);
-        return Pets::create([
-            'name'=>request("name"),
-            'species'=>request("species"),
-            'age'=>request("age"),
-            'gender'=>request("gender"),
-            'adopted'=>request("adopted"),
-            'picturePath'=>request("picturePath"),
-        ]);
+        $image = Input::file('image');
+        if ($image) {
+            $filename = date('Y-m-d-H:i:s')."-".$image->name;
+            Image::make($image->getRealPath())->resize(500, 500)->save('public/images/'.request("name").date('Y-m-d-H:i:s').'/'.$filename);
+        }
+        // return Pets::create([
+        //     'name'=>request("name"),
+        //     'species'=>request("species"),
+        //     'age'=>request("age"),
+        //     'gender'=>request("gender"),
+        //     'adopted'=>request("adopted"),
+        //     'picture_path'=>request("picture_path"),
+        // ]);
     }
     public function update(Request $request, $id){
         $query = Pets::find($id)->update([
@@ -41,7 +54,7 @@ class PetsController extends BaseController
                 'age'=>request("age"),
                 'gender'=>request("gender"),
                 'adopted'=>request("adopted"),
-                'picturePath'=>request("picturePath"),
+                'picture_path'=>request("picture_path"),
         ]);
         if($query){
         return response()->json([
@@ -57,6 +70,17 @@ class PetsController extends BaseController
                 'message' => 'An error occurred!'
             );
             return response()->json($returnData, 500);
+        }
+    }
+    public function storeImage(Request $request){
+        $input = $request->all();
+        if($request->hasFile('image')){
+            $destination_path = 'public/images';
+            $image = $request->file('image');
+            $image_name = $image->getClientOriginalName();
+            $path = $request->file('image')->storeAs($destination_path,$image_name);
+            $input['image']= $image_name;
+            return 'public/images/'.$image_name;
         }
     }
 }
