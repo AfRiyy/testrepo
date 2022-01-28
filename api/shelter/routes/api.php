d5582a26276636a541958f2b4937775a86089cfd
AfRiyy <madaraszdavidd@gmail.com>
2022-01-28 13:31:08 +0100

    api útvonalak

--- api/shelter/routes/api.php
+++ api/shelter/routes/api.php
@@ -20,7 +20,31 @@ use Symfony\Component\HttpFoundation\Cookie;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::group(["middleware" => ["auth:sanctum"]], function() {
+    Route::post('/logout', [AuthController::class, "signOff"]);
+
+    Route::post('/pets',[PetsController::class,'create']);
+    Route::put('/pets/{pet}',[PetsController::class,'update']);
+    Route::delete('/pets/{pet}',[PetsController::class,'delete']);
+
+    Route::post('/species',[SpecieController::class,'create']);
+    Route::put('/species/{specie}',[SpecieController::class,'update']);
+    Route::delete('/species/{specie}',[SpecieController::class,'delete']);
+    
+    Route::post('/breeds',[BreedController::class,'create']);
+    Route::put('/breeds/{breed}',[BreedController::class,'update']);
+    Route::delete('/breeds/{breed}',[BreedController::class,'delete']);
+});
+Route::post('/login', [AuthController::class, "signIn"]);
+Route::post('/register', [AuthController::class, "signUp"]);
+
 Route::get('/pets',[PetsController::class,'index']);
-Route::post('/pets',[PetsController::class,'create']);
-Route::put('/pets/{pet}',[PetsController::class,'update']);
-Route::delete('/pets/{pet}',[PetsController::class,'delete']);
+Route::get('/pets/{pet}',[PetsController::class,'show']);
+
+Route::get('/species',[SpecieController::class,'index']);
+Route::get('/species/{specie}',[SpecieController::class,'show']);
+
+Route::get('/breeds',[BreedController::class,'index']);
+Route::get('/breeds/{breed}',[BreedController::class,'show']);
+
+
