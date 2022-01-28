4a426b810441c890197b09aa3142723797751c71
AfRiyy <madaraszdavidd@gmail.com>
2022-01-28 13:32:26 +0100

    shelters tábla migrációs fájl

---
+++ api/shelter/database/migrations/2022_01_25_153799_create_shelters_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSheltersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shelters', function (Blueprint $table) {
+            $table->engine = "InnoDb";
+            $table->charset = "utf8mb4";
+            $table->collation = "utf8mb4_hungarian_ci";
+            $table->id();
+            $table->string('shelter_name');
+            $table->string('shelter_address');
+            $table->string('shelter_phone');
+            $table->string('shelter_website');
+            $table->string('shelter_facebook');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shelters');
+    }
+}
