205d68bbb09fff7bcdbfcee4c9846046e4ccaff9
AfRiyy <madaraszdavidd@gmail.com>
2022-01-28 13:33:11 +0100

    users tábla migrációs fájl

--- api/shelter/database/migrations/2014_10_12_000000_create_users_table.php
+++ api/shelter/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('fullName');
+            $table->string('username')->unique();
+            $table->string('fullName')->nullable();
+            $table->date('birth_date')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('phone')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
