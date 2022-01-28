51f3f394cf6685fa5dfa576c07915e9f83036fec
AfRiyy <madaraszdavidd@gmail.com>
2022-01-28 13:41:24 +0100

    egyéb

--- api/shelter/routes/web.php
+++ api/shelter/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\PetsController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/pictureTest',function () {
+    return view('picture');
+});
+Route::post('/store',[PetsController::class,'storeImage']);
