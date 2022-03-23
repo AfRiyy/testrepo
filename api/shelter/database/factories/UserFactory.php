85aa8baf725fa71fc9ba43521e9b2c4928c048f0
AfRiyy <madaraszdavidd@gmail.com>
2022-03-23 21:04:41 +0100

    username user-re javítva

--- api/shelter/database/factories/UserFactory.php
+++ api/shelter/database/factories/UserFactory.php
@@ -15,7 +15,7 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'username' => $this->faker->userName(),
+            'user' => $this->faker->userName(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'birth' => $this->faker->date(),
