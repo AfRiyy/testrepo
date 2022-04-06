40ddb8ae4595e4f5d68a26c9b1e9223e19b15ae9
AfRiyy <madaraszdavidd@gmail.com>
2022-04-06 09:18:31 +0200

    apróbb változtatások

--- api/shelter/routes/api.php
+++ api/shelter/routes/api.php
@@ -24,24 +24,24 @@ Route::group(["middleware" => ["auth:sanctum"]], function () {
     Route::post('/logout', [AuthController::class, "signOff"]);
 
     Route::post('/pets', [PetController::class, 'create']);
-    Route::put('/pets/{pet}', [PetController::class, 'update']);
-    Route::delete('/pets/{pet}', [PetController::class, 'delete']);
+    Route::put('/pets/{id}', [PetController::class, 'update']);
+    Route::delete('/pets/{id}', [PetController::class, 'delete']);
 
     Route::post('/species', [SpecieController::class, 'create']);
-    Route::put('/species/{specie}', [SpecieController::class, 'update']);
-    Route::delete('/species/{specie}', [SpecieController::class, 'delete']);
+    Route::put('/species/{id}', [SpecieController::class, 'update']);
+    Route::delete('/species/{id}', [SpecieController::class, 'delete']);
 
     Route::post('/breeds', [BreedController::class, 'create']);
-    Route::put('/breeds/{breed}', [BreedController::class, 'update']);
-    Route::delete('/breeds/{breed}', [BreedController::class, 'delete']);
+    Route::put('/breeds/{id}', [BreedController::class, 'update']);
+    Route::delete('/breeds/{id}', [BreedController::class, 'delete']);
 
     Route::post('/shelters', [ShelterController::class, 'create']);
-    Route::put('/shelters/{shelter}', [ShelterController::class, 'update']);
-    Route::delete('/shelters/{shelter}', [ShelterController::class, 'delete']);
+    Route::put('/shelters/{id}', [ShelterController::class, 'update']);
+    Route::delete('/shelters/{id}', [ShelterController::class, 'delete']);
 
     Route::post('/adoptions', [AdoptionController::class, 'create']);
-    Route::put('/adoptions/{adoption}', [AdoptionController::class, 'update']);
-    Route::delete('/adoptions/{adoption}', [AdoptionController::class, 'delete']);
+    Route::put('/adoptions/{id}', [AdoptionController::class, 'update']);
+    Route::delete('/adoptions/{id}', [AdoptionController::class, 'delete']);
 });
 Route::post('/login', [AuthController::class, "signIn"]);
 Route::post('/register', [AuthController::class, "signUp"]);
@@ -50,13 +50,13 @@ Route::get('/pets', [PetController::class, 'index']);
 Route::get('/pets/{pet}', [PetController::class, 'show']);
 
 Route::get('/species', [SpecieController::class, 'index']);
-Route::get('/species/{specie}', [SpecieController::class, 'show']);
+Route::get('/species/{id}', [SpecieController::class, 'show']);
 
 Route::get('/breeds', [BreedController::class, 'index']);
-Route::get('/breeds/{breed}', [BreedController::class, 'show']);
+Route::get('/breeds/{id}', [BreedController::class, 'show']);
 
 Route::get('/shelters', [ShelterController::class, 'index']);
-Route::get('/shelters/{shelter}', [ShelterController::class, 'show']);
+Route::get('/shelters/{id}', [ShelterController::class, 'show']);
 
 Route::get('/adoptions', [AdoptionController::class, 'index']);
-Route::get('/adoptions/{adoption}', [AdoptionController::class, 'show']);
+Route::get('/adoptions/{id}', [AdoptionController::class, 'show']);
