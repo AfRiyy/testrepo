dd74584ab7927d765dac3ec27d3e2d67fcc8e0cb
Lehoczki Patrícia <lehoczkipatricia99@gmail.com>
2022-04-27 22:42:53 +0200

    menhely kezelése

--- web/fogadj-orokbe/src/app/admin/manageshelters/manageshelters.component.ts
+++ web/fogadj-orokbe/src/app/admin/manageshelters/manageshelters.component.ts
@@ -24,20 +24,19 @@ export class ManagesheltersComponent implements OnInit {
       shelter_zip: new FormControl(''),
       shelter_city: new FormControl(''),
       shelter_street_address: new FormControl(''),
-      shelters_phone: new FormControl(''),
+      shelter_phone: new FormControl(''),
       shelter_website: new FormControl(''),
       shelter_facebook: new FormControl('')
     });
     this.updateShelterForm = new FormGroup({
       id: new FormControl(''),
-      name: new FormControl(''),
-      bname: new FormControl(''),
-      age: new FormControl(''),
-      gender: new FormControl(''),
-      shelters_id: new FormControl(''),
-      neutered: new FormControl(''),
-      sname: new FormControl(''),
-      image: new FormControl('')
+      shelter_name: new FormControl(''),
+      shelter_zip: new FormControl(''),
+      shelter_city: new FormControl(''),
+      shelter_street_address: new FormControl(''),
+      shelter_phone: new FormControl(''),
+      shelter_website: new FormControl(''),
+      shelter_facebook: new FormControl('')
     });
     
   }
@@ -74,6 +73,48 @@ export class ManagesheltersComponent implements OnInit {
       })
   }
 
-  updateShelter(){}
+  onEdit(shelter: any) {
+    this.updateShelterForm.controls['id'].setValue(shelter.id);
+    this.updateShelterForm.controls['shelter_name'].setValue(shelter.shelter_name);
+    this.updateShelterForm.controls['shelter_city'].setValue(shelter.shelter_city);
+    this.updateShelterForm.controls['shelter_zip'].setValue(shelter.shelter_zip);
+    this.updateShelterForm.controls['shelter_street_address'].setValue(shelter.shelter_street_address);
+    this.updateShelterForm.controls['shelter_phone'].setValue(shelter.shelter_phone);
+    this.updateShelterForm.controls['shelter_website'].setValue(shelter.shelter_website);
+    this.updateShelterForm.controls['shelter_facebook'].setValue(shelter.shelter_facebook);
+  }
+
+  updateShelter(){
+    let id = this.updateShelterForm.value.id;
+    let shelter_name = this.updateShelterForm.value.shelter_name;
+    let shelter_zip = this.updateShelterForm.value.shelter_zip;
+    let shelter_city = this.updateShelterForm.value.shelter_city;
+    let shelter_street_address = this.updateShelterForm.value.shelter_street_address;
+    let shelter_phone = this.updateShelterForm.value.shelter_phone;
+    let shelter_website = this.updateShelterForm.value.shelter_website;
+    let shelter_facebook = this.updateShelterForm.value.shelter_facebook;  
+
+    this.shelt.updateShelter(id, shelter_name, shelter_zip, shelter_city, shelter_street_address, shelter_phone, shelter_website, shelter_facebook)
+    .subscribe(res=>{
+      if (res != 0) {
+        alert("Sikeres módosítás");
+        window.location.reload();
+      } else {
+        alert("A módosítás sikertelen!");
+      }
+    })
+  }
+
+  deleteShelter(id:any){
+    this.shelt.deleteShelter(id)
+    .subscribe(res => {
+      if (res != 0) {
+        alert("Sikeres törlés");
+        window.location.reload();
+      } else {
+        alert("A törlés sikertelen!");
+      }
+    })
+  }
 
 }
